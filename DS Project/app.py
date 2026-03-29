d35ab0844d85be2eea564ed53f0094b5cd7c7b23
AqilRahimi <aqilrahimi14@gmail.com>
2026-03-30 00:10:42 +0800

    Update app.py

--- DS Project/app.py
+++ DS Project/app.py
@@ -10,6 +10,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import RandomizedSearchCV
 
+pip install --upgrade pip
+
 # ==========================================
 # PAGE CONFIGURATION
 # ==========================================
